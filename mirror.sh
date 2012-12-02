00703b7316e99b2e7677f098c42e619f5ed6c630
johannes@debussy <johannes@debussy>
2012-12-02 17:37:54 +0100

    mirror script

---
+++ mirror.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+declare -A n
+
+if false; then
+n[c]=C
+n[f77]="FORTRAN 77"
+n[java]=Java
+fi
+n[m]=MATLAB
+n[r]=R
+n[cpp]=C++
+n[f]="FORTRAN 90"
+n[math]=Mathematica
+n[py]=Python
+
+for i in "${!n[@]}"
+do
+		s="${i}_src"
+		src="http://people.sc.fsu.edu/~jburkardt/$s/$s.html"
+		repo="jburkardt-$i"
+		
+		#curl -u 'johannesgerer' https://api.github.com/user/repos -d @-
+		cat <<EOF
+{"name":"$repo"
+,"description":"An official Git Mirror of John Burkardt's great collection of ${n[$i]} Software"
+,"homepage":"$src"
+}
+EOF
+
+if !(cd "$repo"); then
+		git clone git@ghj:johannesgerer/$repo
+fi
+(cd $repo && (\
+#				wget -m -np --cut-dirs=2 -nH $src &&						\
+				cp "${s}.html" README.md &&												\
+				git add -A &&																		\
+				git commit -m "automatic update from $src" &&		\
+		    git gc																				 && \
+				git push origin master
+				)
+)
+done
+
+
