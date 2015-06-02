9fb9ee6cb96ad95469527cb48032778d449b918d
johannes@debussy <johannes@debussy>
2015-06-03 01:44:06 +0200

    automatic update from http://people.sc.fsu.edu/~jburkardt/f77_src/f77_src.html

--- mirror.sh
+++ mirror.sh
@@ -2,16 +2,18 @@
 declare -A n
 
 n[c]=C
-n[f77]="FORTRAN 77"
-n[java]=Java
 n[m]=MATLAB
-n[r]=R
 n[cpp]=C++
 n[f]="FORTRAN 90"
-n[math]=Mathematica
 n[py]=Python
 
-for i in "${!n[@]}"
+n[f77]="FORTRAN 77"
+n[r]=R
+n[math]=Mathematica
+n[java]=Java
+
+echo  "${!n[@]}"
+for i in "${!n[@]}" # "$@"
 do
 		s="${i}_src"
 		src="http://people.sc.fsu.edu/~jburkardt/$s/$s.html"
@@ -29,12 +31,18 @@ if !(cd "$repo"); then
 		git clone git@ghj:johannesgerer/$repo
 fi
 (cd $repo && (\
-#				wget -m -np --cut-dirs=2 -nH $src &&						\
-				cp "${s}.html" README.md &&												\
-				git add -A &&																		\
-				git commit -m "automatic update from $src" &&		\
-		    git gc																				 && \
-				git push origin master
+				# wget "$src" -O /dev/null \
+				# 		&& echo rm \* -rf \
+				# 		&& (echo "Copyright John Burkardt unless indicated differently.\n\nAll rights reserved\n\nEvery subfolder is published under its own License found in the respective folder's *.html files" \
+				# 						 >>  LICENSE) &&
+				# 		wget -m -np --cut-dirs=2 -nH "$src" \
+				# 		&& pandoc  "${s}.html" -t markdown_github -o README.md \
+				# 				&&				 echo OK
+				echo a \
+				&& git add -A															 \
+				&& git commit -m "automatic update from $src" \
+		    && git gc																		 \
+				&& git push origin master
 				)
 )
 done
